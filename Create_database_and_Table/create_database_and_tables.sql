f3088685677418c59ecf4db043fa21426a4e2f65
monirzkhan <odvutkabbo@gmail.com>
2026-06-14 03:21:29 +0600

    data insertation into tables

--- Create_database_and_Table/create_database_and_tables.sql
+++ Create_database_and_Table/create_database_and_tables.sql
@@ -1,35 +1,29 @@
 create database Football_Ticket_Booking_System;
-
-CREATE TABLE users (
+  
+CREATE TABLE userssers (
   user_id serial primary key,
   full_name varchar(50) not null,
   email varchar(50) unique not null,
   role varchar(50) not null check (role in ('Ticket Manager', 'Football Fan')),
   phone_number varchar(20)
+ 
 );
 
 CREATE TABLE matches (
-  match_id serial primary key,
-  fixture varchar(50) not null,
-  tournament_category varchar(50) not null,
-  base_ticket_price int not null check (base_ticket_price >= 0),
-  match_status varchar(50) not null check (
-    match_status in (
-      'Available',
-      'Selling Fast',
-      'Sold Out',
-      'Postponed'
-    )
-  )
+    match_id serial primary key,
+    fixture varchar(50) not null,
+    tournament_category varchar(50) not null,
+    base_ticket_price numeric(10,2)  check (base_ticket_price >=0),
+    match_status varchar(50)  check(match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
 
+
+  
 CREATE TABLE bookings (
-  booking_id serial primary key,
-  user_id int references users (user_id),
-  match_id int references matches (match_id),
-  seat_number varchar(10) not null,
-  payment_status varchar(50) not null check (
-    payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
-  ),
-  total_cost int not null check (total_cost >= 0)
+    booking_id serial primary key,
+    user_id int references users(user_id),
+    match_id int references matches(match_id),
+    seat_number varchar(10) ,
+    payment_status varchar(50) check(payment_status in ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost numeric(10,2) not null check (total_cost >=0)
 )
