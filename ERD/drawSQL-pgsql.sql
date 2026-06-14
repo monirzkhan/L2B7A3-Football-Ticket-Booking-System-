cda36201da8ad8a8e9e0f1c78e9cee0b2fc19fd4
monirzkhan <odvutkabbo@gmail.com>
2026-06-14 17:57:07 +0600

    erd updated

--- ERD/drawSQL-pgsql.sql
+++ ERD/drawSQL-pgsql.sql
@@ -2,8 +2,11 @@ CREATE TABLE "Users"(
     "user_id" SERIAL NOT NULL,
     "full_name" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
-    "role" VARCHAR(255) NOT NULL,
-    "phone_number" VARCHAR(255) NULL
+    "role" VARCHAR(255) CHECK
+        (
+            "role" IN('Ticket Manager', 'Football Fan')
+        ) NOT NULL,
+        "phone_number" VARCHAR(255) NULL
 );
 ALTER TABLE
     "Users" ADD PRIMARY KEY("user_id");
@@ -13,8 +16,16 @@ CREATE TABLE "Matches"(
     "match_id" SERIAL NOT NULL,
     "fixture" VARCHAR(255) NOT NULL,
     "tournament_category" VARCHAR(255) NOT NULL,
-    "base_ticket_price" INTEGER NOT NULL,
-    "match_status" VARCHAR(255) NOT NULL
+    "base_ticket_price" DECIMAL(8, 2) NOT NULL,
+    "match_status" VARCHAR(255) CHECK
+        (
+            "match_status" IN(
+                'Available',
+                'Selling Fast',
+                'Sold Out',
+                'Postponed'
+            )
+        ) NOT NULL
 );
 ALTER TABLE
     "Matches" ADD PRIMARY KEY("match_id");
@@ -23,8 +34,16 @@ CREATE TABLE "Bookings"(
     "user_id" INTEGER NOT NULL,
     "match_id" INTEGER NOT NULL,
     "seat_number" VARCHAR(255) NULL,
-    "payment_status" VARCHAR(255) NULL,
-    "total_cost" INTEGER NOT NULL
+    "payment_status" VARCHAR(255) CHECK
+        (
+            "payment_status" IN(
+                'Pending',
+                'Confirmed',
+                'Cancelled',
+                'Refunded'
+            )
+        ) NULL,
+        "total_cost" DECIMAL(8, 2) NOT NULL
 );
 ALTER TABLE
     "Bookings" ADD PRIMARY KEY("booking_id");
